2f0a6e2c56e2c13f95c67ab6e27a3c59d308fe4c
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-08-12 19:33:25 +0200

    [UPD] milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 function App() {
 
   const [politicians, setPoliticians] = useState([]);
+  const [query, setQuery] = useState('');
+  const [specificPolitician, setSpecificPolitician] = useState([])
 
   async function getObj(url) {
     const res = await fetch(url);
@@ -18,22 +20,60 @@ function App() {
     })();
   }
 
+  const showSpecificPolitician = (element) => {
+    const person = politicians.filter((p) => {
+      return p.name.toLowerCase().includes(query.trim().toLowerCase()) || p.biography.toLowerCase().includes(query.trim().toLowerCase())
+
+    })
+    setSpecificPolitician(person)
+    setQuery('')
+  }
+
   useEffect(() => {
     showCards()
   }, [])
 
   console.log(politicians)
+  console.log(specificPolitician)
 
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-12 d-flex justify-content-center">
-            <h1>Politicians</h1>
+            <h1 onClick={() => { setSpecificPolitician([]) }}>Politicians</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center">
+            <input type="text"
+              placeholder='Search politician...'
+              value={query}
+              onChange={(e) => { setQuery(e.target.value) }} />
+            <button onClick={() => { showSpecificPolitician(query) }}>Search</button>
           </div>
         </div>
         <div className="card-container d-flex flex-wrap p-2n justify-content-center">
-          {politicians.map((politician) => {
+          {specificPolitician?.length > 0 ? specificPolitician.map((s) => {
+            return <div key={s.id} className="card">
+              <div className="card-img-top">
+                <img className='img-fluid' src={s.image
+
+                } />
+              </div>
+              <div className="card-body">
+                <div className="card-title">
+                  <h3>{s.name}</h3>
+                </div>
+                <div className="card-text">
+                  <p><strong>Position:  </strong>{s.position
+                  }</p>
+                  <p><strong>Biography:  </strong>{s.biography
+                  }</p>
+                </div>
+              </div>
+            </div>
+          }) : politicians.map((politician) => {
             return <div key={politician.id} className="card">
               <div className="card-img-top">
                 <img className='img-fluid' src={politician.image
